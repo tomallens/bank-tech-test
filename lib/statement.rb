c590e90df4609b0a713300aa7f551b8f0c793ee8
Tom <tomoliverallen@gmail.com>
2022-11-16 13:27:23 +0000

    Statement Formats Transactions and Time
    Potentially worth splitting the Statement class in future
    to handle formatting - I/O class for expansion?

--- lib/statement.rb
+++ lib/statement.rb
@@ -8,4 +8,14 @@ class Statement
   def print_header
     "date || credit || debit || balance"
   end
+
+  def date_formatter
+    Date.today.strftime('%d/%m/%Y')
+  end
+
+  def column_formatter(transaction_type, amount)
+    transaction = '%.2f' % amount
+
+    transaction_type == 'deposit' ? " #{transaction} || " : " || #{transaction} "
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,19 +1,32 @@
 require_relative '../lib/statement.rb'
+require 'date'
 
 RSpec.describe Statement do
 
   before(:each) do 
     reset_statement
+    allow(Date).to receive(:today).and_return Date.new(2022,11,15)
   end
 
   def reset_statement
-    @statement = statement.new
+    @statement = Statement.new(0)
   end
 
- 
   it 'should print the header' do
-    @statement.print_header
 
     expect(@statement.print_header).to eq "date || credit || debit || balance"
+  end
+
+  it 'formats the date into dd/mm/yyyy' do
+  
+    expect(@statement.date_formatter).to eq "15/11/2022"
+  end
+
+  context 'when given a transaction' do
+    it 'formats the credit/debit columns correctly' do
+
+      expect(@statement.column_formatter('deposit', 200)).to eq " 200.00 || "
+      expect(@statement.column_formatter('withdrawal', 100)).to eq " || 100.00 "
+    end
   end
 end
